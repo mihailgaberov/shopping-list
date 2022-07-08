f8edebcb8de2b381606228653ccf371d52f64fb6
mihail.gaberov <mihail.gaberov@ysura.com>
2022-07-08 08:16:14 +0200

    ✨ Show if someone is typing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import { useOthers } from "./liveblocks.config";
+import { useOthers, useUpdateMyPresence } from "./liveblocks.config";
+import { useState } from "react";
 
 function WhoIsHere() {
   const others = useOthers();
@@ -11,10 +12,42 @@ function WhoIsHere() {
   );
 }
 
+function SomeoneIsTyping() {
+  const someoneIsTyping = useOthers()
+    .toArray()
+    .some((user) => user.presence?.isTyping);
+
+  return (
+    <div className="someone_is_typing">
+      {someoneIsTyping ? "Someone is typing..." : ""}
+    </div>
+  );
+}
+
 export default function App() {
+  const [draft, setDraft] = useState("");
+  const updateMyPresence = useUpdateMyPresence();
+
   return (
     <div className="container">
       <WhoIsHere />
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        value={draft}
+        onChange={(e) => {
+          setDraft(e.target.value);
+          updateMyPresence({ isTyping: true });
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            updateMyPresence({ isTyping: false });
+            setDraft("");
+          }
+        }}
+        onBlur={() => updateMyPresence({ isTyping: false })}
+      />
+      <SomeoneIsTyping />
     </div>
   );
 }
