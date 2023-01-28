21762ddaacba7588dcbf618c64eaa38ce005d31c
mihailgaberov <mihail.gaberov@gmail.com>
2023-01-28 16:14:51 +0100

    :mute: Removed redundant logs

--- src/components/SomeoneIsTyping/SomeoneIsTyping.jsx
+++ src/components/SomeoneIsTyping/SomeoneIsTyping.jsx
@@ -6,8 +6,6 @@ export function SomeoneIsTyping() {
     .toArray()
     .some((user) => user.presence?.isTyping);
 
-  console.log(">>> someoneIsTyping", someoneIsTyping)
-
   return (
     <div className={styles.container}>
       {someoneIsTyping ? "Someone is typing..." : ""}
