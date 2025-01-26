1908ce967dc3766f5f60621eecac61f97dbd702b
mihailgaberov <mihail.gaberov@gmail.com>
2025-01-26 11:02:53 +0100

    refactor: remove Liveblocks client configuration from liveblocks.config.js and integrate it directly in main.jsx

--- src/liveblocks.config.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@liveblocks/client";
-
-const API_KEY = import.meta.env.VITE_API_KEY;
-
-const client = createClient({
-  publicApiKey: API_KEY,
-});
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import { RoomProvider } from "@liveblocks/react";
-import { LiveList } from "@liveblocks/client"
+import { LiveList } from "@liveblocks/client";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <RoomProvider
-            id="shoppingList"
-            initialStorage={{ groceries: new LiveList([]) }}
-            initialPresence={{ groceries: [] }}>
-            <App/>
-        </RoomProvider>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <LiveblocksProvider
+      publicApiKey={API_KEY}
+      authEndpoint="/api/liveblocks-auth"
+      throttle={16}
+    >
+      <RoomProvider
+        id="shoppingList"
+        initialStorage={{ groceries: new LiveList([]) }}
+        initialPresence={{ groceries: [] }}
+      >
+        <App />
+      </RoomProvider>
+    </LiveblocksProvider>
+  </React.StrictMode>
+);
